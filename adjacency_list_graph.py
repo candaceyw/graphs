8cc716ffc10ffe8b873aef03c57c977394254a0e
candaceyw <candaceyw@gmail.com>
2020-08-17 11:11:17 -0400

    init commit

---
+++ adjacency_list_graph.py
@@ -0,0 +1,21 @@
+class Vertex:
+
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+
+    def add_neighbor(self, nbr, weight=0):
+        self.connectedTo[nbr] = weight
+
+    def get_connections(self):
+        return self.connectedTo.keys()
+
+    def get_id(self):
+        return self.id
+
+    def get_weight(self, nbr):
+        return self.connectedTo[nbr]
+
+    def __str__(self):
+        return str(self.id) + ' connected to: ' + str([x.id for x in self.connectedTo])
+    
