59ed6bd79980f6f51f112279db4ad6c25c8d1edf
candaceyw <candaceyw@gmail.com>
2020-08-17 16:28:37 -0400

    saving

--- word_ladder.py
+++ word_ladder.py
@@ -53,15 +53,40 @@ class Graph:
         return iter(self.vertList.values())
 
 
-g = Graph()
-for i in range(6):
-    g.addVertex(i)
+# g = Graph()
+# for i in range(6):
+#     g.addVertex(i)
+#
+# g.vertList
+#
+# g.addEdge(0, 1, 2)
+#
+# for vertex in g:
+#     print(vertex)
+#     print(vertex.getConnections())
+#     print('\n')
 
-g.vertList
 
-g.addEdge(0, 1, 2)
+def buildGraph(wordFile):
+    d = {}
+    g = Graph()
 
-for vertex in g:
-    print(vertex)
-    print(vertex.getConnections())
-    print('\n')
+    wfile = open(wordFile, 'r')
+    # create buckets of words that differ by one letter
+    for line in wfile:
+        print(line)
+        word = line[:-1]
+        print(word)
+        for i in range(len(word)):
+            bucket = word[:i] + '_' + word[i + 1:]
+            if bucket in d:
+                d[bucket].append(word)
+            else:
+                d[bucket] = [word]
+    # add vertices and edges for words in the same bucket
+    for bucket in d.keys():
+        for word1 in d[bucket]:
+            for word2 in d[bucket]:
+                if word1 != word2:
+                    g.addEdge(word1, word2)
+    return g
